8e545b1055bd946ef970511e7956b7082e91d94c
David <Rigby335@hotmail.com>
2022-04-27 10:59:38 -0700

    finished warmups

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const colors = ['red', 'blue', 'green', 'yellow']
 
-console.log("============ Question 1 ============")
+console.log(colors[1]);
 // Using its index value in colors, console.log 'blue'
 
-console.log("============ Question 2 ============")
+colors.push("purple"); // this is a call on the array
+console.log(colors);
+console.log(colors[4]);
 // add the value 'purple' to the end of the colors array
 // console.log the result
 
@@ -13,10 +15,16 @@ const benny = {
   breeds: ['Terrier', 'Chihauhau', 'Pekingese']
 }
 
-console.log("============ Question 3 ============")
+console.log(benny.name);
+console.log(benny.age);
+console.log(benny.name,benny.age);
 // Using dot notation, console.log the name and age of Benny
 
-console.log("============ Question 4 ============")
+console.log(benny.breeds[1]);
+// what if i wanted to loop through all benny's names and loop through them
+for (let breed of benny.breeds){
+  console.log(breed);
+}
 // Using dot notation, console.log any one of Benny's breeds
 
 console.log("============ Question 5 ============")
@@ -24,8 +32,13 @@ console.log("============ Question 5 ============")
 // and returns an all lower case version
 
 function whisper(word){
-  // Your code here
+  return word.toLowerCase(); // this is a call on the array
 }
+// return values are like when you place an order for a burger the return is getting the burger back
+// functions are like machines on a belt where you
+//  we're putting in a word with all uppercase and getting all lowercase
+// toLowerCase is a string method
+// method means you actually call it using a . on the data type itself
 
 console.log(`this should print true: ${whisper('Hello') === 'hello'}`)
 console.log(`this should print true: ${whisper('BOOGERS') === 'boogers'}`)
@@ -48,12 +61,15 @@ const dogs = [{
   age: 4,
   breeds: ['Unknown Mix']
 }]
-console.log("============ BONUS -- 1 ============")
-// Using index values and dot notation, console.log each dog's name
 
+console.log(dogs[0].name);
+console.log(dogs[1].name);
+//
+// Using index values and dot notation, console.log each dog's name
+console.log(dogs[0].breeds[0]);
 console.log("============ BONUS -- 2 ============")
 // Using index values and dot notation, console.log the first breed of each dog
-
+console.log(`${dogs[0].name} is ${dogs[0].age} years old`);
 console.log("============ BONUS -- 3 ============")
 // Using template literals, print the following string
 // Benny is 6 years old
